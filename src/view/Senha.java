ab7b223e102af2fdd2cc54b9af35c45e492927af
bruno venancio <jbrunogvenancio@gmail.com>
2020-09-29 23:37:05 -0300

    trabalho

---
+++ src/view/Senha.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+import java.util.ArrayList;
+/**
+ *
+ * @author Usuário 1
+ */
+public class Senha {
+    ArrayList<String> lista = new ArrayList<>();
+    String tipo;
+    int numero;
+}
